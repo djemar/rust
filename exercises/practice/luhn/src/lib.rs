3bf3de345a84e90255ef821885d5f5974048d1e4
Diego Marino <marinodiego.96@gmail.com>
2022-03-23 16:29:13 +0100

    implement luhn lib

--- exercises/practice/luhn/src/lib.rs
+++ exercises/practice/luhn/src/lib.rs
@@ -1,4 +1,25 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {} valid?", code);
+    let mut luhn = 0;
+    let mut doubling;
+    let cleaned_code = code.replace(" ", "").chars().rev().collect::<String>();
+    let mut j = 1;
+    if cleaned_code.chars().count() < 2 { return false; }
+
+    for ch in cleaned_code.chars() {
+        match ch {
+            ' ' => continue,
+            '0'..='9' => {
+                if j%2 == 0 {
+                    doubling = ch.to_digit(10).unwrap() * 2;
+                    if doubling > 9 { doubling -= 9; }
+                    luhn += doubling;
+                }
+                else { luhn += ch.to_digit(10).unwrap(); }
+            }
+            _ => return false,
+        }
+        j += 1;
+    }
+    luhn % 10 == 0
 }
